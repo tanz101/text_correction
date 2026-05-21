288c0d5e3332dddbdb32bbcf58f662b88a6ff766
tanz101 <tzielin1@gmail.com>
2026-05-21 15:07:45 -0500

    remove bot env file

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 # Load environment variables
-load_dotenv()
+load_dotenv('C:\\Users\\tziel\\Documents\\Git\\.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
 POINTS_FILE = 'discord_bot/points.json'
 
